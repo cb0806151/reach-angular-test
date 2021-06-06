b20ff0e1fb9155995d6cecc2760031b326fc428e
cb0806151 <cb0806151@otc.edu>
2021-06-06 08:28:47 -0500

    update with minimal version that uses the new setup requirements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,26 +4,36 @@ const reach = loadStdlib('ALGO');
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <div>
+      <button (click)="connectWallet()">connect wallet</button>
+      <div>
+        <input [(ngModel)]="fundAmount"/>
+        <button (click)="fundWallet()">fund wallet</button>
+      </div>
+    </div>
+  `,
+  styles: []
 })
 export class AppComponent {
-  title = 'reachAngularTest';
   account: any;
   balance: any;
-
+  fundAmount: number = 0;
   async connectWallet() {
     await this.getAccount();
     await this.getBalance();
   }
-
   async getAccount() {
-    this.account = await reach.getDefaultAccount("testWallet");
+    this.account = await reach.getDefaultAccount();
     console.log(this.account);
   }
-
   async getBalance() {
-    this.balance = reach.formatCurrency(await reach.balanceOf(this.account), 4);
+    let rawBalance = await reach.balanceOf(this.account)
+    this.balance = reach.formatCurrency(rawBalance, 4);
     console.log(this.balance);
+  }
+  async fundWallet() {
+    await reach.fundFromFaucet(this.account, reach.parseCurrency(this.fundAmount));
+    await this.getBalance();
   }
 }
